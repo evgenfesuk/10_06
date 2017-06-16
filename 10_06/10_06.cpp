2b5a237fa50c68c495437f493bf94be36f6b1a29
evgen.fesuk <evgen.fesuk@gmail.com>
2017-06-16 19:57:32 +0300

    while str1.lenght == str2.length working

--- 10_06/10_06.cpp
+++ 10_06/10_06.cpp
@@ -2,10 +2,18 @@
 //
 
 #include "stdafx.h"
+#include "compStr.h"
+#include <iostream>
 
 
 int main()
 {
+	char* str1 = "AAAA";
+	char* str2 = "AAA";
+	int result;
+	result = compStr(str1, str2);
+	std::cout << result << std::endl;
+	system("pause");
     return 0;
 }
 
--- 10_06/compStr.cpp
+++ 10_06/compStr.cpp
@@ -1,7 +1,20 @@
+#include "stdafx.h"
 #include "compStr.h"
+#include <iostream>
 
-int compStr(char *, char *)
+int compStr(char *str1, char *str2)
 {
+	int length;
+	std::strlen(str1) > std::strlen(str2) ? length = std::strlen(str1) : length = std::strlen(str2);
 
+	for (int i = 0; i < length; i++)
+	{
+		if (*(str1 + i) > *(str2 + i))	return 1;
+	}
+
+	for (int i = 0; i < length; i++)
+	{
+		if (*(str1 + i) < *(str2 + i))	return -1;
+	}
 	return 0;
 }
