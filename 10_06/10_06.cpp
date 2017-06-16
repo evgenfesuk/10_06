40329cba8a2e9e8664cbab74edca1266ec2ddc41
evgen.fesuk <evgen.fesuk@gmail.com>
2017-06-16 20:22:47 +0300

    working app

--- 10_06/10_06.cpp
+++ 10_06/10_06.cpp
@@ -8,10 +8,9 @@
 
 int main()
 {
-	char* str1 = "AAAA";
-	char* str2 = "AAA";
-	int result;
-	result = compStr(str1, str2);
+	char* str1 = "a";
+	char* str2 = "aaa";
+	int result = compStr(str1, str2);
 	std::cout << result << std::endl;
 	system("pause");
     return 0;
